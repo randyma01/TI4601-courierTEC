a3d8ac4422e5394f484609d30a2c3a89c957ca0c
Randy <randyma01@gmail.com>
2018-09-12 15:41:50 -0600

    Trabajo en script central v1.

--- scripts/base_central/courier_sql_server_v1.sql
+++ scripts/base_central/courier_sql_server_v1.sql
@@ -34,6 +34,20 @@ CREATE TABLE Provincia(
 );
 GO
 
+-- Tabla de los CLientes --
+CREATE TABLE Clientes(
+	IdCliente INT NOT NULL PRIMARY KEY,
+	Cedula INT NOT NULL, 						-- esta prodria ser la llave primaria ¿?
+	Nombre VARCHAR(15) NOT NULL,
+	Apellido VARCHAR(15) NOT NULL,
+	Telefono VARCHAR(11) NOT NULL,
+	-- Tipo VARCHAR(11) NOT NULL 		-- puede ser un emun(bronce, oro, platino)
+	FechaNacimiento DATE NOT NULL,
+	IdPronvincia INT NOT NULL,
+	FOREIGN KEY (IdProvincia) REFERENCES Provincia(IdProvincia)
+);
+GO
+
 -- Tabla de los Administradores --
 CREATE TABLE Administrador(
 	IdAdmin INT IDENTITY PRIMARY KEY,
@@ -52,25 +66,43 @@ CREATE TABLE Sucursal(
 );
 GO
 
+-- Tabla de los Paquetes --
+CREATE TABLE Paquete(
+	IdPaquete INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	FechaIngreso DATE NOT NULL,
+	Tipo VARCHAR(30) NOT NULL
+	Descripcion VARCHAR(MAX) NOT NULL,
+	Peso INT NOT NULL,
+	Precio INT NOT NULL,
+	-- Estados VARCHAR (20) NOT NULL, 		-- puede ser un emun(en camino, en sucursal, retirado)
+);
+GO
+
+-- Tabla de los Clientes por Paquete --
+CREATE TABLE Cliente_Paquete(
+	IdCliente INT NOT NULL,
+	IdPaquete INT NOT NULL,
+	Monto INT NOT NULL,
+	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
+	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete),
+);
+GO
+
+-- Tabla de los Paquetes por Sucursal --
+CREATE TABLE Cliente_Paquete(
+	IdSucursal INT NOT NULL,
+	IdPaquete INT NOT NULL,
+	Monto INT NOT NULL,
+	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
+);
+GO
+
 -- Tabla de los Administradores por Sucurscal --
 CREATE TABLE Admi_Sucursal(
 	IdAdmin INT NOT NULL,
 	IdSucursal INT NOT NULL,
 	FOREIGN KEY (IdAdmin) REFERENCES Administrador(IdAdmin),
-	FOREIGN KEY (IdAdmin) REFERENCES Sucursal(IdSucursal),
-);
-GO
-
--- Tabla de los CLientes --
-CREATE TABLE Clientes(
-	IdCliente INT NOT NULL PRIMARY KEY,
-	Cedula INT NOT NULL, 						-- esta prodria ser la llave primaria ¿?
-	Nombre VARCHAR(15) NOT NULL,
-	Apellido VARCHAR(15) NOT NULL,
-	Telefono VARCHAR(11) NOT NULL,
-	-- Tipo VARCHAR(11) NOT NULL 		-- Puede ser un emun(bronce, oro, platino)
-	FechaNacimiento DATE NOT NULL,
-	IdPronvincia INT NOT NULL,
-	FOREIGN KEY (IdProvincia) REFERENCES Provincia(IdProvincia)
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
 );
 GO
