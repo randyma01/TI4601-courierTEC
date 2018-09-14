959ffd26dafab9240359c37af74575b9fbb54213
Randy <randyma01@gmail.com>
2018-09-13 23:43:08 -0600

    nodo cartago v1.

--- scripts/base_cartago/cartago_sql_server_v1.sql
+++ scripts/base_cartago/cartago_sql_server_v1.sql
@@ -6,22 +6,34 @@ Bases de Datos Avanzados, Área Acádemica de Administración de Tecnología de 
 Semestre II, 2018
 
 Miembros:
-	Karla -
-	Maria Paula -
-	Hazel -
-	Randy Martínez Sandí - 2014047395
+	Karla Araya Corrales
+	Maria Paula Ramírez Ortiz
+	Hazel Arias Abarca
+	Randy Martínez Sandí
+
+Nota:
+	Este es el script para la distribución del nodo Cartago de la base
+	central courierTEC.
 
 Índice:
 		1- Creación de las tablas
-    2- Declaración de los sp, triggers.
+		2- Fragmentación de las tablas de couriertecDB
 */
 
 --------------------------------------------------------
 ----------Inicio: Creación de la base de datos----------
 --------------------------------------------------------
 
-CREATE DATABASE couriertecDB;
+CREATE DATABASE couriertecCartagoDB;
 GO
+
+---------------------------------------------------
+--------------Uso de la base de datos--------------
+---------------------------------------------------
+
+USE couriertecCartagoDB;
+GO
+
 
 --------------------------------------------------
 ----------Inicio: Creación de las tablas----------
@@ -29,40 +41,53 @@ GO
 
 -- Tabla de las Provincias --
 CREATE TABLE Provincia(
-	IdProvincia INT NOT NULL IDENTITY(1,1) PRIMARY KEY
+	IdProvincia INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre VARCHAR (12)
 );
 GO
 
 -- Tabla de los CLientes --
-CREATE TABLE Clientes(
-	IdCliente INT NOT NULL PRIMARY KEY,
-	Cedula INT NOT NULL, 						-- esta prodria ser la llave primaria ¿?
+CREATE TABLE Cliente(
+	IdCliente INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Cedula INT NOT NULL,
 	Nombre VARCHAR(15) NOT NULL,
 	Apellido VARCHAR(15) NOT NULL,
 	Telefono VARCHAR(11) NOT NULL,
-	-- Tipo VARCHAR(11) NOT NULL 		-- puede ser un emun(bronce, oro, platino)
+	Tipo VARCHAR(11) NOT NULL, 					-- puede ser (bronce, oro, platino)
 	FechaNacimiento DATE NOT NULL,
-	IdPronvincia INT NOT NULL,
+	IdProvincia INT NOT NULL,
 	FOREIGN KEY (IdProvincia) REFERENCES Provincia(IdProvincia)
-);
-GO
-
--- Tabla de los Administradores --
-CREATE TABLE Administrador(
-	IdAdmin INT IDENTITY PRIMARY KEY,
-	Nombre VARCHAR(15) NOT NULL,
-	Apellido VARCHAR(15) NOT NULL,
-	Cedula INT NOT NULL,							-- esta prodria ser la llave primaria ¿?
 );
 GO
 
 -- Tabla de las Sucursales --
 CREATE TABLE Sucursal(
-	IdSucursal NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	IdSucursal INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre VARCHAR(30) NOT NULL,
 	Telefono VARCHAR(11) NOT NULL,
-	Correo VARCHAR(25) NOT NULL,
+	Correo VARCHAR(25) NOT NULL
+);
+GO
+
+-- Tabla de los Empleados --
+CREATE TABLE Empleado(
+	IdEmpleado INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR(15) NOT NULL,
+	Apellido VARCHAR(15) NOT NULL,
+	Cedula INT NOT NULL,
+	IdSucursal INT NOT NULL,
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
+);
+GO
+
+-- Tabla de los Administradores --
+CREATE TABLE Administrador(
+	IdAdmin INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR(15) NOT NULL,
+	Apellido VARCHAR(15) NOT NULL,
+	Cedula INT NOT NULL,
+	IdSucursal INT NOT NULL,
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -70,11 +95,21 @@ GO
 CREATE TABLE Paquete(
 	IdPaquete INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	FechaIngreso DATE NOT NULL,
-	Tipo VARCHAR(30) NOT NULL
-	Descripcion VARCHAR(MAX) NOT NULL,
+	Tipo VARCHAR(30) NOT NULL, 				--(ropa, juegetes, herramientas, etc).
+	Descripcion VARCHAR(1000) NOT NULL,
 	Peso INT NOT NULL,
 	Precio INT NOT NULL,
-	-- Estados VARCHAR (20) NOT NULL, 		-- puede ser un emun(en camino, en sucursal, retirado)
+	Monto INT NOT NULL,
+	EstadoPaquete VARCHAR (20) NOT NULL 		--(en camino, en sucursal, retirado)
+);
+GO
+
+-- Tabla de los Cliente por Sucursal --
+CREATE TABLE Cliente_Sucursal(
+	IdCliente INT NOT NULL,
+	IdSucursal INT NOT NULL,
+	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -82,9 +117,8 @@ GO
 CREATE TABLE Cliente_Paquete(
 	IdCliente INT NOT NULL,
 	IdPaquete INT NOT NULL,
-	Monto INT NOT NULL,
 	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
-	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete),
+	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete)
 );
 GO
 
@@ -92,17 +126,7 @@ GO
 CREATE TABLE Paquete_Sucursal(
 	IdSucursal INT NOT NULL,
 	IdPaquete INT NOT NULL,
-	Monto INT NOT NULL,
 	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
-);
-GO
-
--- Tabla de los Administradores por Sucurscal --
-CREATE TABLE Admi_Sucursal(
-	IdAdmin INT NOT NULL,
-	IdSucursal INT NOT NULL,
-	FOREIGN KEY (IdAdmin) REFERENCES Administrador(IdAdmin),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
