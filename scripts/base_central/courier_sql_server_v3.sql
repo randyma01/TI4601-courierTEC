b1945877d44d24de813a08c001521f1de4f4fcc6
Randy <randyma01@gmail.com>
2018-09-13 21:42:23 -0600

    script v3.

--- scripts/base_central/courier_sql_server_v3.sql
+++ scripts/base_central/courier_sql_server_v3.sql
@@ -35,7 +35,7 @@ CREATE TABLE Provincia(
 GO
 
 -- Tabla de los CLientes --
-CREATE TABLE Clientes(
+CREATE TABLE Cliente(
 	IdCliente INT NOT NULL PRIMARY KEY,
 	Cedula INT NOT NULL,
 	Nombre VARCHAR(15) NOT NULL,
@@ -54,6 +54,8 @@ CREATE TABLE Empleado(
 	Nombre VARCHAR(15) NOT NULL,
 	Apellido VARCHAR(15) NOT NULL,
 	Cedula INT NOT NULL,
+	IdSucursal INT NOT NULL,
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -63,6 +65,8 @@ CREATE TABLE Administrador(
 	Nombre VARCHAR(15) NOT NULL,
 	Apellido VARCHAR(15) NOT NULL,
 	Cedula INT NOT NULL,
+	IdSucursal INT NOT NULL,
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -71,7 +75,7 @@ CREATE TABLE Sucursal(
 	IdSucursal NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre VARCHAR(30) NOT NULL,
 	Telefono VARCHAR(11) NOT NULL,
-	Correo VARCHAR(25) NOT NULL,
+	Correo VARCHAR(25) NOT NULL
 );
 GO
 
@@ -84,7 +88,7 @@ CREATE TABLE Paquete(
 	Peso INT NOT NULL,
 	Precio INT NOT NULL,
 	Monto INT NOT NULL,
-	EstadoPaquete VARCHAR (20) NOT NULL, 		--(en camino, en sucursal, retirado)
+	EstadoPaquete VARCHAR (20) NOT NULL 		--(en camino, en sucursal, retirado)
 );
 GO
 
@@ -93,7 +97,7 @@ CREATE TABLE Cliente_Sucursal(
 	IdCliente INT NOT NULL,
 	IdSucursal INT NOT NULL,
 	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -102,7 +106,7 @@ CREATE TABLE Cliente_Paquete(
 	IdCliente INT NOT NULL,
 	IdPaquete INT NOT NULL,
 	FOREIGN KEY (IdCliente) REFERENCES Cliente(IdCliente),
-	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete),
+	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete)
 );
 GO
 
@@ -111,25 +115,7 @@ CREATE TABLE Paquete_Sucursal(
 	IdSucursal INT NOT NULL,
 	IdPaquete INT NOT NULL,
 	FOREIGN KEY (IdPaquete) REFERENCES Paquete(IdPaquete),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
-);
-GO
-
--- Tabla de los Administradores por Sucurscal --
-CREATE TABLE Admi_Sucursal(
-	IdAdmin INT NOT NULL,
-	IdSucursal INT NOT NULL,
-	FOREIGN KEY (IdAdmin) REFERENCES Administrador(IdAdmin),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
-);
-GO
-
--- Tabla de los Empleados por Sucursal --
-CREATE TABLE Empleado_Surcursal(
-	IdEmpleado INT NOT NULL,
-	IdSucursal INT NOT NULL,
-	FOREIGN KEY (IdEmpleado) REFERENCES Empleado(IdEmpleado),
-	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal),
+	FOREIGN KEY (IdSucursal) REFERENCES Sucursal(IdSucursal)
 );
 GO
 
@@ -143,9 +129,53 @@ INSERT INTO Provincia (Nombre) VALUES
 GO
 
 -- Llenado tabla  de los Administradores --
-INSERT INTO Administrador(Nombre, Apellido, Cedula) VALUES
-('Hazel', 'Arias', 123456378),
-('Karla', 'Araya', 178540922),
-('Paula', 'Ramírez', 177339922),
-('Randy', 'Martínez', 100729944);
+INSERT INTO Administrador(Nombre, Apellido, Cedula, IdSucursal) VALUES
+('Hazel', 'Arias', 123456378, 1),
+('Karla', 'Araya', 178540922, 2),
+('Paula', 'Ramírez', 177339922, 1),
+('Randy', 'Martínez', 100729944, 2);
+GO
+
+-- Llenando tabla de Clientes --
+INSERT INTO Cliente (Cedula, Nombre, Apellido, Telefono, Tipo, FechaNacimiento, IdProvincia) VALUES
+(89894527, 'Max', 'Green', '89815637', 'bronce', GETDATE(), 2),
+(56473829, 'Xav', 'Red', '89815637', 'oro', GETDATE(), 3),
+(12344321, 'Tim', 'Blue', '89815637', 'platino', GETDATE(), 5),
+(89765739, 'Sen', 'Yell', '89815637', 'bronce', GETDATE(), 1);
+GO
+
+-- Llenado tabla  de los Empleado --
+INSERT INTO Empleado(Nombre, Apellido, Cedula, IdSucursal) VALUES
+('Sof', 'Cor', 112233444),
+('Est', 'Sol', 888775553),
+('Dan', 'Car', 908909889),
+('Ang', 'Rod', 098734122);
+GO
+
+-- Llenado tabla de los Sucursal --
+INSERT INTO Sucursal (Nombre, Telefono, Correo) VALUES
+(,'Sede San Jose', 78764532, 'csj@couriertec.com' ),
+(,'Sede Cartago', 23432123, 'cca@couriertec.com'),
+
+-- Llenado tablas de los Paquetes --
+INSERT INTO Sucursarl (FechaIngreso, Tipo, Descripcion, Peso, Precio, Monto, EstadoPaquete) VALUES
+(GETDATE(), 'Ropa', 'Tennis corredor', 2, 35000, 50000, 'Camino'),
+(GETDATE(), 'Juegos', 'Juego de mesa', 1, 15000, 30000, 'Sucursar'),
+(GETDATE(), 'Ropa', 'Jeans', 4, 30000, 80000, 'Sucursal'),
+(GETDATE(), 'Electronico', 'Pantalla', 20, 1000000, 400000, 'Retirado');
+GO
+
+-- Llenado tabla de los Clientes por Sucursal --
+INSET INTO Cliente_Sucursal (IdCliente, IdSucursal) VALUES
+(1,1), (2,1), (3,2), (4,2);
+GO
+
+-- Llenado tabla de los Clientes por Paquetes --
+INSET INTO Cliente_Sucursal (IdCliente, IdSucursal) VALUES
+(1,1), (2,2), (3,3), (4,4);
+GO
+
+-- Llenado tabla de los Paquetes por Sucursal --
+INSET INTO Paquete_Sucursal (IdPaquete, IdSucursal) VALUES
+(1,1), (2,1), (3,2), (4,2);
 GO
