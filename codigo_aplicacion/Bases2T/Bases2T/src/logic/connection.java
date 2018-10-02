596c68e3df5d224ef65ba64df4beddd731ebeef6
Randy <randyma01@gmail.com>
2018-10-01 23:42:45 -0600

    pruebas finales

--- codigo_aplicacion/Bases2T/Bases2T/src/logic/connection.java
+++ codigo_aplicacion/Bases2T/Bases2T/src/logic/connection.java
@@ -31,7 +31,7 @@ public class connection {
         System.out.println(url);
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connectionCT = DriverManager.getConnection(url); 
+            connectionCT = DriverManager.getConnection(url);
             String msg = "Conecto";
             System.out.println(msg);
             JOptionPane.showMessageDialog(null,msg);
@@ -39,19 +39,19 @@ public class connection {
         catch (Exception e) {
             System.out.println(e);
         }
-        return connectionCT ;     
+        return connectionCT ;
     }
     public Connection Cnn(String Database){
         String Server = "localhost";
         //String Database = "couriertecDB";
         String url = "jdbc:sqlserver://"+Server+":1433"
-                + ";database="+Database+";user=feo;password=feo123;SSL:false"
+                + ";database="+Database+";user=randyma;password=Alex2004;SSL:false"
                 + "encrypt=true;trustServerCertificate=false;loginTimeout=30;";
         url = String.format(url);
        // System.out.println(url);
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connectionCT = DriverManager.getConnection(url); 
+            connectionCT = DriverManager.getConnection(url);
             String msg = "Conecto";
             System.out.println(msg);
           //  JOptionPane.showMessageDialog(null,msg);
@@ -59,15 +59,15 @@ public class connection {
         catch (Exception e) {
             System.out.println(e);
         }
-        return connectionCT ;     
+        return connectionCT ;
     }
     public Connection getConn(){
         return connectionCT;
     }
     public String sendQuery(String query,String Database,int num){
-        Connection cnn = Cnn(Database);       
+        Connection cnn = Cnn(Database);
         Statement stmt;
-        String result = ""; 
+        String result = "";
         System.out.println(query);
         try {
             stmt = cnn.createStatement();
@@ -76,15 +76,15 @@ public class connection {
              while (resultSet.next()){
                 if (num ==3 ){
                   result = result + " \n " + resultSet.getString(1) + " "+ resultSet.getString(2)
-                  + resultSet.getString(3);  
+                  + resultSet.getString(3);
                 }
                 else if (num == 2){
-                  result = result + " \n " + resultSet.getString(1) + " "+ resultSet.getString(2);  
+                  result = result + " \n " + resultSet.getString(1) + " "+ resultSet.getString(2);
                 }
                 else{
                     result = result + " \n " + resultSet.getString(1) ;
                 }
-                
+
              }
             System.out.println(result);
         } catch (SQLException e) {
@@ -100,8 +100,8 @@ public class connection {
             DefaultTableModel tablam = new DefaultTableModel();
             System.out.println(query);
             clearTable(tablam);
-            Connection  cnn = Cnn(database);  
-            Statement statement = cnn.createStatement(); 
+            Connection  cnn = Cnn(database);
+            Statement statement = cnn.createStatement();
             ResultSet rs = statement.executeQuery(query);
             tablam.addColumn("ID Paquete");
             tablam.addColumn("Tipo");
@@ -110,9 +110,9 @@ public class connection {
             tablam.addColumn("Precio");
             tablam.addColumn("Estado");
             tablam.addColumn("Sucursal");
-            
+
          while (rs.next()) {
-            String data[]  = {rs.getString(1), rs.getString(2), rs.getString(3) 
+            String data[]  = {rs.getString(1), rs.getString(2), rs.getString(3)
             ,rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7)};
             tablam.addRow(data);
         }
@@ -121,22 +121,22 @@ public class connection {
          catch (Exception e){
             System.out.println(e);
         }
-            
+
     }
-    
+
     public void clearTable(DefaultTableModel tab){
         for (int i = tab.getRowCount()-1 ; i >=0; i--){
             tab.removeRow(i);
         }
-        
+
     }
-    
+
     public Boolean logIn(String User,String ID){
         Boolean value = false;
-        Connection cnn = Cnn("couriertecDB");       
+        Connection cnn = Cnn("couriertecDB");
         Statement stmt;
         String query = "Select Cedula, Nombre From " + User ;
-        String result = ""; 
+        String result = "";
         System.out.println(query);
         try {
             stmt = cnn.createStatement();
@@ -150,7 +150,7 @@ public class connection {
                     System.out.println("Esta en la Base de Datos");
                     msg = msg + resultSet.getString(2);
                     JOptionPane.showMessageDialog(null,msg);
-                }   
+                }
              }
         }
               catch (SQLException e) {
